c0418f0df96e1210e412ebb20e4be569269d584d
Петренков Николай <@corp.itstep>
2017-12-28 10:18:37 +0600

    Работа 6(28.12.17)

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Person person = new Person() { FirstName = "Nikolai", LastName = "Petrenkov" };
+            ClassLibrary1.Class1.PrintInfo(person);
+            Constances constances = new Constances();
+            Console.WriteLine(constances.GetInt());
+            Console.WriteLine(constances.GetString());
+            Console.ReadLine();
+        }
+    }
+}
